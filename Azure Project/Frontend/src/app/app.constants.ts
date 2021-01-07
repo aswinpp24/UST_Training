0cd28dfd8e8e1428aba446ab6c4a0413166d958d
Aswin P P <aswin@armino.in>
2021-01-07 21:13:29 +0530

    Azure Project added

---
+++ Azure Project/Frontend/src/app/app.constants.ts
@@ -0,0 +1,8 @@
+
+//export const API_URL = 'http://localhost:7071/api/todos';
+//export const API_URL = 'https://ust-todo-functions-app.azurewebsites.net/api/todos';
+export const API_URL = 'https://ust-todos-api-management.azure-api.net/ust-todo-functions-app/todos';
+export const POOL_DATA = {
+    UserPoolId: 'us-east-1_GMdZm52Wh',//'us-east-1_7dF78dyvw',
+    ClientId: '7h6ivmhtdrfduhu4jjvfjqtduo'//'7pn13e127fqnptfp0glb3bke8e'
+};
